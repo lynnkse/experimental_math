429480c29d7179af08df4fb1b625ed4d43d34ef2
Anton Gulyaev <lynnkse@gmail.com>
2023-03-09 18:57:12 +0200

    asd

--- math_functions/src/math_functions.jl
+++ math_functions/src/math_functions.jl
@@ -1,15 +1,45 @@
 
-module MathFunctions
+module VectorSpaces
 
-export add, multiply
+# Define an abstract type for vectors
+abstract type AbstractVector end
 
-function add(x, y)
-    return x + y
+# Define an interface for vector addition
+abstract type AbstractVectorAddition end
+
+# Define an interface for scalar multiplication
+abstract type AbstractScalarMultiplication end
+
+# Define an abstract type for vector spaces
+abstract type AbstractVectorSpace end
+
+# Define an interface for field addition
+abstract type AbstractFieldAddition end
+
+# Define an interface for field multiplication
+abstract type AbstractFieldMultiplication end
+
+# Define an abstract type for fields
+abstract type AbstractField end
+
+# Define a concrete type for vectors that implement addition
+struct AdditionVector{T<:AbstractVectorAddition} <: AbstractVector
+    data::T
 end
 
-function multiply(x, y)
-    return x * y
+# Define a concrete type for vectors that implement scalar multiplication
+struct ScalarMultiplicationVector{T<:AbstractScalarMultiplication} <: AbstractVector
+    data::T
 end
 
+# Define a concrete type for vector spaces that implement addition and scalar multiplication
+struct VectorSpace{T1<:AbstractVectorAddition, T2<:AbstractScalarMultiplication} <: AbstractVectorSpace
+    addition::T1
+    scalar_multiplication::T2
 end
 
+# Define a concrete type for fields that implement addition and multiplication
+struct Field{T1<:AbstractFieldAddition, T2<:AbstractFieldMultiplication} <: AbstractField
+    addition::T1
+    multiplication::T2
+end
