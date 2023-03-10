c0e0fdf2c261667935682ac40a528c8460bf090e
Anton Gulyaev <lynnkse@gmail.com>
2023-03-10 19:41:47 +0200

    fields

--- math_functions/src/math_functions.jl
+++ math_functions/src/math_functions.jl
@@ -23,23 +23,66 @@ abstract type AbstractFieldMultiplication end
 abstract type AbstractField end
 
 # Define a concrete type for vectors that implement addition
-struct AdditionVector{T<:AbstractVectorAddition} <: AbstractVector
-    data::T
-end
+# struct AdditionVector{T<:AbstractVectorAddition} <: AbstractVector
+#     data::T
+# end
 
 # Define a concrete type for vectors that implement scalar multiplication
-struct ScalarMultiplicationVector{T<:AbstractScalarMultiplication} <: AbstractVector
+# struct ScalarMultiplicationVector{T<:AbstractScalarMultiplication} <: AbstractVector
+#     data::T
+# end
+
+# Define the field
+struct NumericField{T<:Number} <: AbstractField{T} # AbstractField{T<:Number} ??
+    add::(T,T) -> T
+    mul::(T,T) -> T
+    zero::T
+    one::T
+end
+
+struct MyField{T} <: AbstractField{T}
     data::T
 end
+# MyField should not compile if some functions required by its interface are not defined from global scope. MyField constructor will only accept one parameter
+# what does it mean if the functions are available?
 
-# Define a concrete type for vector spaces that implement addition and scalar multiplication
-struct VectorSpace{T1<:AbstractVectorAddition, T2<:AbstractScalarMultiplication} <: AbstractVectorSpace
-    addition::T1
-    scalar_multiplication::T2
+#Vector2DField, we would need to either define the required binary operations and identities for Vector2D outside of the class 
+#definition and pass them in as arguments, or define them directly inside the class definition.
+
+#Could there be a field over a data type that requires only addition but not multiplication?
+
+function add(x::Float64, y::Float64)
+    return x + y
 end
 
-# Define a concrete type for fields that implement addition and multiplication
-struct Field{T1<:AbstractFieldAddition, T2<:AbstractFieldMultiplication} <: AbstractField
-    addition::T1
-    multiplication::T2
+function mul(x::Float64, y::Float64)
+    return x * y
 end
+
+function zero_el{T<:Number}()
+    return zero(T)
+end
+
+function one_el{T<:Number}()
+    return one(T)
+end
+
+# Create an instance of NumericField for Float64
+# field_float64 = NumericField{Float64}(add, mul, zero_el, one_el)
+
+
+
+
+# # Define the vector space
+# struct MyVectorSpace{T<:AbstractFloat} <: AbstractVectorSpace
+#     field::MyField{T}
+#     dimension::Int
+# end
+
+# # Define a concrete type for vector spaces that implement addition and scalar multiplication
+# struct VectorSpace{T1<:AbstractVectorAddition, T2<:AbstractScalarMultiplication} <: AbstractVectorSpace
+#     addition::T1
+#     scalar_multiplication::T2
+# end
+
+
